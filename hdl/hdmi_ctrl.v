8472d4f35e69c91f6c62556073d8965401ee8f56
Faakka <bottika9@gmail.com>
2021-12-27 15:47:03 +0100

    timing added, hdmi_ctrl before test

--- hdl/hdmi_ctrl.v
+++ hdl/hdmi_ctrl.v
@@ -33,26 +33,121 @@ module hdmi_ctrl(
    output wire       tmds_data2_out_p,    //Adat 2.
    output wire       tmds_data2_out_n,
    output wire       tmds_clock_out_p,    //Pixel órajel.
-   output wire       tmds_clock_out_n
+   output wire       tmds_clock_out_n,
+
+    (* X_INTERFACE_INFO = "xilinx.com:interface:axis:1.0 S_AXI TDATA" *)
+    input [31:0] tdata,
+    (* X_INTERFACE_INFO = "xilinx.com:interface:axis:1.0 S_AXI TLAST" *)
+    input tlast,
+    (* X_INTERFACE_INFO = "xilinx.com:interface:axis:1.0 S_AXI TVALID" *)
+    input tvalid,
+    (* X_INTERFACE_INFO = "xilinx.com:interface:axis:1.0 S_AXI TREADY" *)
+    output tready
 );
 
-wire [7:0] red;
-wire [7:0] green;
-wire [7:0] blue;
-wire       blank;
-wire       hsync;
-wire       vsync;
+    /*
+    * Erdemes egy kulon modult csinalni a sync- es blank jelek generalasahoz
+    * A 640x480 @ 60 Hz kriteriumai:
+    *   - 25(,175) MHz pixelorajel
+    *   - horizontal:
+    *       - front porch: 16
+    *       - sync pulse : 96
+    *       - back porch : 48
+    *   - vertical:
+    *       - front porch: 10
+    *       - sync pulse : 2
+    *       - back porch : 33
+    *   - teljes frame: 800x525
+    */
+
+
+reg [7:0] red_r;
+reg [7:0] green_r;
+reg [7:0] blue_r;
+
+wire [7:0] red_w;
+wire [7:0] green_w;
+wire [7:0] blue_w;
+
+reg blank_r;
+reg hsync_r;
+reg vsync_r;
+reg frame_end_r;
+
+wire blank_w;
+wire hsync_w;
+wire vsync_w;
+wire frame_end_w;
+
+reg [1:0] state = 0;
+
+localparam INIT = 2'b00;
+localparam SYNC = 2'b01;
+localparam DISP = 2'b10;
+
+always @(posedge clk) begin
+    if(rst) begin
+        state <= INIT;
+    end
+    else begin
+        case(state)
+            INIT: begin
+                if(tlast)
+                    state <= SYNC;
+                else
+                    state <= INIT;
+            end
+
+            SYNC: begin
+                if(frame_end_r) begin
+                    if(tvalid)
+                        state <= DISP;
+                    else
+                        state <= INIT;
+                end
+                else
+                    state <= SYNC;
+            end
+
+            DISP: begin
+                if(tlast | ~tvalid)
+                    state <= SYNC;
+                else
+                    state <= DISP;
+            end
+        endcase
+    end
+end
+
+always @(posedge clk) begin
+    if(state == DISP) begin
+        red_r <= tdata[7:0];
+        green_r <= tdata[15:8];
+        blue_r <= tdata[23:16];
+    end
+    else begin
+        red_r <= 0;
+        green_r <= 0;
+        blue_r <= 0;
+    end
+end
+
+assign tready = ~blank_r & ((state == INIT) || (state == DISP));
+
+assign red_w = blank_r ? 7'b0 : red_r;
+assign green_w = blank_r ? 7'b0 : green_r;
+assign blue_w = blank_r ? 7'b0 : blue_r;
 
 tmds_transmitter TMDS_TX(
     .clk(clk),
     .clk_5x(clk_5x),
     .rst(rst),
-    .red_in(red),
-    .green_in(green),
-    .blue_in(blue),
-    .blank_in(blank),
-    .hsync_in(hsync),
-    .vsync_in(vsync),
+    .red_in(red_w),
+    .green_in(green_w),
+    .blue_in(blue_w),
+    .blank_in(blank_r),
+    .hsync_in(hsync_r),
+    .vsync_in(vsync_r),
 
    .tmds_data0_out_p(tmds_data0_out_p),
    .tmds_data0_out_n(tmds_data0_out_n),
@@ -64,7 +159,30 @@ tmds_transmitter TMDS_TX(
    .tmds_clock_out_n(tmds_clock_out_n)
 );
 
+vga_timing TIMING(
+    .clk(clk),
+    .rst(rst),
+    .h_cnt(),
+    .v_cnt(),
+    .h_sync(hsync_w),
+    .v_sync(vsync_w),
+    .frame_end(frame_end_w),
+    .blank(blank_w)
+);
 
-
+always @(posedge clk) begin
+    hsync_r <= hsync_w;
+    vsync_r <= vsync_w;
+    frame_end_r <= frame_end_w;
+    blank_r <= blank_w;
+end
 
 endmodule
+
+
+
+
+
+
+
+
---
+++ hdl/vga_timing.v
@@ -0,0 +1,136 @@
+`timescale 1ns / 1ps
+`default_nettype none
+
+//******************************************************************************
+//* 640 x 480 @ 60 Hz VGA idõzítés generátor.                                 *
+//******************************************************************************
+module vga_timing (
+   //Órajel és reset.
+   input  wire        clk,             //Pixel órajel bemenet.
+   input  wire        rst,             //Reset bemenet.
+   
+   //Az aktuális pixel pozíció.
+   output reg  [10:0] h_cnt = 11'd0,   //X-koordináta.
+   output reg  [9:0]  v_cnt = 10'd0,   //Y-koordináta.
+   
+   //Szinkron és kioltó jelek.
+   output reg         h_sync = 1'b1,   //Horizontális szinkron pulzus.
+   output reg         v_sync = 1'b0,   //Vertikális szinkron pulzus.
+   output reg         frame_end = 1'b0,
+   output wire        blank            //Kioltó jel.
+);
+
+//******************************************************************************
+//* Idõzítési paraméterek.                                                     *
+//******************************************************************************
+localparam H_VISIBLE     = 640;
+localparam H_FRONT_PORCH = 16;
+localparam H_SYNC_PULSE  = 96;
+localparam H_BACK_PORCH  = 48;
+
+localparam V_VISIBLE     = 480;
+localparam V_FRONT_PORCH = 10;
+localparam V_SYNC_PULSE  = 2;
+localparam V_BACK_PORCH  = 33;
+
+localparam H_BLANK_BEGIN = H_VISIBLE     - 1;
+localparam H_SYNC_BEGIN  = H_BLANK_BEGIN + H_FRONT_PORCH;
+localparam H_SYNC_END    = H_SYNC_BEGIN  + H_SYNC_PULSE;
+localparam H_BLANK_END   = H_SYNC_END    + H_BACK_PORCH;
+
+localparam V_BLANK_BEGIN = V_VISIBLE     - 1;
+localparam V_SYNC_BEGIN  = V_BLANK_BEGIN + V_FRONT_PORCH;
+localparam V_SYNC_END    = V_SYNC_BEGIN  + V_SYNC_PULSE;
+localparam V_BLANK_END   = V_SYNC_END    + V_BACK_PORCH;
+
+
+//******************************************************************************
+//* A horizontális és vertikális számlálók.                                    *
+//******************************************************************************
+always @(posedge clk)
+begin
+   if (rst || (h_cnt == H_BLANK_END))
+      h_cnt <= 12'd0;
+   else
+      h_cnt <= h_cnt + 12'd1;
+end
+
+always @(posedge clk)
+begin
+   if (rst)
+      v_cnt <= 11'd0;
+   else
+      if (h_cnt == H_BLANK_END)
+         if (v_cnt == V_BLANK_END)
+            v_cnt <= 11'd0;
+         else
+            v_cnt <= v_cnt + 11'd1;
+end
+
+
+//******************************************************************************
+//* A szinkron pulzusok generálása.                                            *
+//******************************************************************************
+always @(posedge clk)
+begin
+   if (rst || (h_cnt == H_SYNC_END))
+      h_sync <= 1'b1;
+   else
+      if (h_cnt == H_SYNC_BEGIN)
+         h_sync <= 1'b0;
+end
+
+always @(posedge clk)
+begin
+   if (rst)
+      v_sync <= 1'b0;
+   else
+      if (h_cnt == H_BLANK_END)
+         if (v_cnt == V_SYNC_BEGIN)
+            v_sync <= 1'b1;
+         else
+            if (v_cnt == V_SYNC_END)
+               v_sync <= 1'b0;
+end
+
+
+//******************************************************************************
+//* A kioltó jel elõállítása.                                                  *
+//******************************************************************************
+reg h_blank = 1'b0;
+reg v_blank = 1'b0;
+
+always @(posedge clk)
+begin
+   if (rst || (h_cnt == H_BLANK_END))
+      h_blank <= 1'b0;
+   else
+      if (h_cnt == H_BLANK_BEGIN)
+         h_blank <= 1'b1;
+end
+
+always @(posedge clk)
+begin
+   if (rst)
+      v_blank <= 1'b0;
+   else
+       if (h_cnt == H_BLANK_END) begin
+            if (v_cnt == V_BLANK_END - 1'b1)
+                frame_end <= 1'b1;
+            else
+                frame_end <= 1'b0;
+
+            if (v_cnt == V_BLANK_BEGIN)
+                v_blank <= 1'b1;
+            else
+                if (v_cnt == V_BLANK_END)
+                    v_blank <= 1'b0;
+       end
+end
+
+assign blank = h_blank | v_blank;
+
+endmodule 
+
+`default_nettype wire
+
